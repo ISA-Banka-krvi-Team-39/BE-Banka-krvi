12e32a70cf14f69490dc615205d813cca7fdc443
unknown <milic.ra208.2019@uns.ac.rs>
2022-12-20 22:58:24 +0100

    implemented get all terms

--- src/main/java/app/center/service/ITermService.java
+++ src/main/java/app/center/service/ITermService.java
@@ -2,7 +2,11 @@ package app.center.service;
 
 import app.center.model.Term;
 
+import java.util.List;
+
 public interface ITermService {
     public Term create(Term term);
     public Term findOne(Integer id);
+    public List<Term> getAll();
+
 }
